4e663e573975e1efeec119729e5f55b9cb130ebe
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-03-20 10:44:34 +0100

    added configs

---
+++ bv/kh/generate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+export OMP_NUM_THREADS=1
+name=kelvinhelmholtz
+
+for p in 0.1 0.075 0.05 0.025 0.01 0.005;
+do
+    for r in 64 128 256 512 1024;
+    do
+	s=$r
+	NH=$(( (64*$r)/2048))
+	mkdir ${name}_${p}_${r};
+	cd ${name}_${p}_${r}
+	cp -r ../${name} ./
+	sed -i "s/NX/${r}/g" ${name}/${name}.xml
+	sed -i "s/NH/${NH}/g" ${name}/${name}.xml
+	sed -i "s/SAMPLES/${s}/g" ${name}/${name}.xml
+	sed -i "s/PERTURBATION/${p}/g" ${name}/${name}.py
+	cp ../submit.sh ./
+	sed -i "s/NODES/$((${r}/2))/g" submit.sh
+	T='24:00'
+
+#	bsub -W $T -n $r  -N -B mpirun -np $r $HOME/alsvinn/build/alsuqcli/alsuqcli ${name}/${name}.xml
+	cd ..;
+    done;
+
+done;
---
+++ bv/kh/kelvinhelmholtz_0.005_512/submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --job-name=kh
+#SBATCH --mail-user=kjetil.lye@sam.math.ethz.ch
+#SBATCH --time=02:00:00
+#SBATCH --nodes=256
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=18
+#SBATCH --partition=normal
+#SBATCH --constraint=mc
+#SBATCH --account=s665
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+srun $HOME/alsvinn/build/alsuqcli/alsuqcli kelvinhelmholtz/kelvinhelmholtz.xml
+
